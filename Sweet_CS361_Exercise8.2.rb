49d7b86f5831ff5ab24173d3cac2fd231dff0011
natex <natexsweet@gmail.com>
2023-11-24 14:03:01 -0800

    refactored code

--- Sweet_CS361_Exercise8.2.rb
+++ Sweet_CS361_Exercise8.2.rb
@@ -1,8 +1,8 @@
 class Poker
-    def initialize(players)
-        @players = players
-        @hands = []
-        players.length().times { |x| @hands.append(nil) }
+    def initialize(args)
+        @players = args[:players] # changed to use args
+        # @hands = [] Removed hands since each player will have a hand
+        players.length().times { |x| @players[x].append_to_hand(self.create_hand()) } # changed to fill the hand item of a player
     end
 
     def play_poker()
@@ -20,18 +20,24 @@ class Poker
     end
 
     def get_player_hand(i)
-        return @hands[i]
+        return @playes[i].get_hand
+    end
+
+    def create_hand()
+        # pretend this pull randomly from a deck removing pulled items from the deck
+        return nil 
     end
 end
 
 class Chess
-    def initialize(players)
-        @players = players
+    def initialize(args)
+        @players = args[:players] #changed to use args
+        @players.length().times { |x| @players[x].set_color(self.pick_color(x)) } # changed to set the color of a player
     end
 
     def play_game()
         puts "Players in the chess game:"
-        @players.length().times { |x| puts "#{self.get_player_name(x)}: #{@players[x][1]}" }
+        @players.length().times { |x| puts "#{self.get_player_name(x)}: #{self.get_player_color(x)}" }
         # [pretend there's code here]
     end
 
@@ -40,15 +46,36 @@ class Chess
     end
 
     def get_player_name(i)
-        @players[i][0]
+        @players[i].name #changed to use the attr_reader
+    end
+
+    def get_player_color(i)
+        @players[i].color # changed to use the attr_reader
+    end
+
+    def pick_color(i)         # picks a color for a player
+        if i == 0
+            return "white"
+        else
+            return "black"
+        end
     end
 end
 
-class GoPlayer
-    attr_reader :name, :color
+class Player
+    attr_reader :name, :color, :hand
 
-    def initialize(name, color)
-        @name = name
+    def initialize(args)        #changed to use args
+        @name = args[:name]
+        @color = nil
+        @hand = []
+    end
+
+    def append_to_hand(cards)       # append the passed to the hand
+        cards.each { |card| @hand.append(card) }
+    end
+
+    def set_color(color)        # set the color of the player
         @color = color
     end
 end
